abc72c981fee5ad5a7838e211d62c63a1fc15c14
mjs0611 <42108687+mjs0611@users.noreply.github.com>
2020-02-25 11:03:53 +0900

    Firlst Crawl Practice

---
+++ section02-1.py
@@ -0,0 +1,33 @@
+# Section02-1
+# 파이썬 크롤링 기초
+# urllib 사용법 및 기본 스크랩핑
+
+import urllib.request as req
+
+# 파일 URL
+img_url = 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20120808_277%2Ftpet2_1344432366023a5JkD_PNG%2F2.png&type=b400'
+html_url = 'https://www.google.com'
+
+# 다운받을 경로
+save_path1 = 'C:/test1.jpg'
+save_path2 = 'C:/index.html'
+
+# 예외 처리
+try:
+    file1, header1 = req.urlretrieve(img_url, save_path1)
+    file2, header2 = req.urlretrieve(html_url, save_path2)
+except Exception as e:
+    print('Download failed')
+    print(e)
+else:
+    # Header 정보 출력
+    print(header1)
+    print(header2)
+
+    # 다운로드 파일 정보
+    print('Filename1 {}'.format(file1))
+    print('Filename2 P{}'.format(file2))
+    print()
+
+    # 성공
+    print('Download Succeed')
---
+++ section02-2.py
@@ -0,0 +1,52 @@
+# Section02-2
+# 파이썬 크롤링 기초
+# urlopen 함수 기초 사용법
+
+import urllib.request as req
+from urllib.error import URLError, HTTPError
+
+# 다운로드 경로 및 파일명
+
+path_list = ["C:/test1.jpg", "C:/index.html"]
+
+# 다운로드 리소스 url
+target_url = ["http://blogfiles.naver.net/MjAxNzExMjZfMTc2/MDAxNTExNjY5Njk0Nzgx.Ab1d1dF2B7N32eiJg7M54Yzu51_hUUcr7u1_m-d2ERQg.5p30egP3gD9h3o0ghVg_mQ0IQYDUTwmp3QczmsyUix8g.JPEG.imuuri/godntalk_com_20170509_091945.jpg", "http://google.com"]
+
+for i, url in enumerate(target_url):
+    # 예외 처리
+    try:
+        # 웹 수신 정보 읽기
+        response = req.urlopen(url)
+
+        # 수신 내용
+        contents = response.read()
+
+        print("-----------------------------------------------------------------------")
+
+        # 상태 정보 중간 출력
+        print('Header Info-{} : {}'.format(i, response.info()))
+        print('HTTP Status Code: {}',format(response.getcode()))
+        print()
+        print("-----------------------------------------------------------------------")
+
+        with open(path_list[i], 'wb') as c:
+            c.write(contents)
+
+
+    except HTTPError as e:
+        print("Download failed")
+        print("HTTPError Code : ", e.code)
+    except URLError as e:
+        print("Download failed")
+        print("URL Error Reason : ", e.reason)
+
+    # 성공
+    else:
+        print()
+        print("Download Succeed.")
+
+
+
+
+
+
